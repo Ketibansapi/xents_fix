68b2ab841170fa98140b9fefc296b5822490d96c
Deary H <deary2706@gmail.com>
2020-04-19 16:33:19 +0800

    firebase auth setup

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,6 +4,21 @@ import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
 import * as firebase from 'firebase'
 
 export default class LoginScreen extends React.Component {
+    state = {
+        email: "",
+        password: "",
+        errorMessage: null
+    };
+
+    handleLogin = () => {
+        const {email, password} = this.state
+
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .catch(error => this.setState({errorMessage: error.message}));
+    };  
+
     render() {
         return (
             <View style={styles.container}>
@@ -12,21 +27,32 @@ export default class LoginScreen extends React.Component {
                 </Text>
 
                 <View style = {styles.errorMessage}>
-                    <Text>Error</Text>
+                    {this.state.errorMessage && <Text style={styles.error}>{this.state.errorMessage}</Text>}
                 </View>
 
                 <View style={styles.form}>
                     <View>
                         <Text style={styles.inputTitle}>Email Address</Text>
-                        <TextInput style={styles.input} autoCapitalize="none"></TextInput>
+                        <TextInput 
+                        style={styles.input} 
+                        autoCapitalize="none" 
+                        onChangeText={email => this.setState({email})}
+                        value={this.state.email}
+                        ></TextInput>
                     </View>
                     <View style={{marginTop: 32}}>
                         <Text style={styles.inputTitle}>Password</Text>
-                        <TextInput style={styles.input} secureTextEntry autoCapitalize="none"></TextInput>
+                        <TextInput 
+                        style={styles.input} 
+                        secureTextEntry 
+                        autoCapitalize="none"
+                        onChangeText={password => this.setState({ password })}
+                        value = {this.state.password}
+                        ></TextInput>
                     </View>
                 </View>
 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity style={styles.button} onPress={this.handleLogin}>
                     <Text style={{ color: "#FFF", fontWeight: "500"}}>Sign in</Text>
                 </TouchableOpacity>
 
@@ -55,6 +81,12 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
         marginHorizontal: 30
+    },
+    error: {
+        color: "#E9446A",
+        fontSize: 13,
+        fontWeight: "600",
+        textAlign: "center"
     },
     form: {
         marginBottom: 48,
